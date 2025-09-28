83793707f0991b0d1b8f8a59d04db78fc0985e5c
mathewsmunemba41@gmail.com <kingjay41>
2025-09-28 06:00:52 +0200

    Day 11: For loops - multiplication table

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,11 +1,11 @@
 num = int (input("multiplication table: "))
 
 print(f"\nMultiplication Table for {num}:")
-for i in range(1, 11):
+for i in range(1, 10):
     print(f"{num} x {i} = {num * i}")
 
 print(" Multiplication Tables from 1 to 5")
-for n in range(1, 6):
+for n in range(1, 5):
     print(f"\nTable for {n}:")
-    for i in range(1, 11):
+    for i in range(1, 10):
         print(f"{n} x {i} = {n * i}")
